60029c2fb1ed036cca867ca724107d136701efea
ylscat <ylscat@gmai.com>
2016-03-18 18:07:01 +0800

    读取通话记录

--- app/src/main/java/com/fangstar/diagnostic/ListFragment.java
+++ app/src/main/java/com/fangstar/diagnostic/ListFragment.java
@@ -1,15 +1,18 @@
 package com.fangstar.diagnostic;
 
-import android.app.Activity;
 import android.app.Fragment;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import static android.provider.CallLog.Calls.CONTENT_URI;
 import static android.provider.CallLog.Calls.DATE;
@@ -23,15 +26,22 @@ import static android.provider.CallLog.Calls._ID;
  *
  * @author YinLanShan
  */
-public class ListFragment extends Fragment {
-    private SimpleCursorAdapter mAdapter;
+public class ListFragment extends Fragment implements View.OnClickListener {
+    private Adapter mAdapter;
+    private View mProgress;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getLoaderManager().initLoader(1, null, mCallbacks);
+    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         if (mAdapter == null) {
-            Activity activity = getActivity();
-            /*if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) !=
+            /*Activity activity = getActivity();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) !=
                     PackageManager.PERMISSION_GRANTED) {
                 // TODO: Consider calling
                 //    ActivityCompat#requestPermissions
@@ -42,13 +52,9 @@ public class ListFragment extends Fragment {
                 // for ActivityCompat#requestPermissions for more details.
                 return;
             }*/
-            mAdapter = new SimpleCursorAdapter(getActivity(),
-                    android.R.layout.simple_list_item_2,
-                    null, new String[]{NUMBER, DATE, DURATION},
-                    new int[]{android.R.id.text1, android.R.id.text2},
-                    SimpleCursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+            mAdapter = new Adapter(getActivity().getLayoutInflater());
             if(getView() != null) {
-                ListView lv = (ListView)getView();
+                ListView lv = (ListView)getView().findViewById(R.id.list);
                 lv.setAdapter(mAdapter);
             }
         }
@@ -57,18 +63,98 @@ public class ListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        ListView listView = new ListView(inflater.getContext());
-        listView.setAdapter(mAdapter);
-        return listView;
+        View view = inflater.inflate(R.layout.logs, container, false);
+        view.findViewById(R.id.button).setOnClickListener(this);
+        mProgress = view.findViewById(R.id.progress);
+        return view;
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
+    public void onDestroy() {
+        super.onDestroy();
+        if(mAdapter != null)
+            mAdapter.swapCursor(null);
+    }
 
-        Cursor c = getActivity().getContentResolver().query(CONTENT_URI,
-                new String[]{NUMBER, DATE, DURATION, _ID}, null, null, DEFAULT_SORT_ORDER);
-        mAdapter.swapCursor(c);
-        mAdapter.notifyDataSetChanged();
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                getLoaderManager().restartLoader(1, null, mCallbacks);
+                break;
+        }
+    }
+
+    private LoaderManager.LoaderCallbacks<Cursor> mCallbacks = new LoaderManager.LoaderCallbacks<Cursor>() {
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            String[] cols = new String[]{NUMBER, DATE, DURATION, _ID};
+            CursorLoader loader = new CursorLoader(getActivity(), CONTENT_URI,
+                    cols, null, null, DEFAULT_SORT_ORDER + " limit 20");
+            return loader;
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+            mAdapter.swapCursor(data);
+            mProgress.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+            mAdapter.swapCursor(null);
+            mProgress.setVisibility(View.VISIBLE);
+        }
+    };
+
+    class Adapter extends BaseAdapter {
+        private Cursor mCursor;
+        private LayoutInflater mInflater;
+
+        public Adapter(LayoutInflater inflater) {
+            mInflater = inflater;
+        }
+
+        @Override
+        public int getCount() {
+            if(mCursor == null)
+                return 0;
+            return mCursor.getCount();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mCursor.moveToPosition(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView == null) {
+                convertView = mInflater.inflate(android.R.layout.simple_list_item_2,
+                        parent, false);
+            }
+
+            Cursor cursor = mCursor;
+            cursor.moveToPosition(position);
+            TextView text = (TextView)convertView.findViewById(android.R.id.text1);
+            text.setText(cursor.getString(0));
+            text = (TextView)convertView.findViewById(android.R.id.text2);
+            text.setText(cursor.getString(1));
+
+            return convertView;
+        }
+
+        public void swapCursor(Cursor cursor) {
+            if(mCursor != null) {
+                mCursor.close();
+            }
+            mCursor = cursor;
+            notifyDataSetChanged();
+        }
     }
 }
